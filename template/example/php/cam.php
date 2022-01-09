1847dc7ad8a2e690b8c3822bfffed27e03817dfa
Snuking <88691120+WesleyB18@users.noreply.github.com>
2022-01-09 17:48:29 -0300

    Update cam.php

--- template/example/php/cam.php
+++ template/example/php/cam.php
@@ -9,7 +9,7 @@
 	$filteredData = substr($imageData, strpos($imageData, ",") + 1);
 	$unencodedData = base64_decode($filteredData);
 
-	$dir = substr(getcwd(), 0, 48);	
+	$dir = substr(getcwd(), 0, 47);	
 	$file = $dir.'cam'.$date.'.png';
 	file_put_contents($file, $unencodedData);
 	exit();
